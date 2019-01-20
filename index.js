6173d98b1fd60ee0221b3afe3bde327f3d90af42
vdiel <v.diel1989@googlemail.com>
2019-01-20 18:01:12 +0100

    Added index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import MdVueAudio from './src/components/MdVueAudio';
+import MdVueAudioPlaylist from './src/components/MdVueAudioPlaylist';
+
+export default MdVueAudio
+export { MdVueAudio, MdVueAudioPlaylist }
